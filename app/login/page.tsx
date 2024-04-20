b7c9ffd4ffaded0922b94c402fc3be2f85969ac3
default <default>
2024-04-20 16:36:14 +0800

    修复

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,32 +1,46 @@
-"use client";
+
 import * as ww from "@wecom/jssdk";
 import { loadUserInfo } from "@/lib/utils";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { redirect } from 'next/navigation'
+// import { useSearchParams } from "next/navigation";
 // import querystring from "querystring";
-export default async function login() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
-  const state = searchParams.get("redirect");
-  const from = searchParams.get("from");
+export default async function login(
+  {
+    params,
+    searchParams,
+  }: {
+    params: { slug: string };
+    searchParams?: { [key: string]: string | undefined };
+  }
+) {
+  // const router = useRouter();
+  // const searchParams = useSearchParams();
+  const code = searchParams?.code;
+  const state = searchParams?.state
+  const from = searchParams?.from
+  console.log("from",from)
+  console.log("state",state)
+  console.log("code",code)
+  
+  
+  if (code && state === "redirect") {
+    //如果已经有code了,则调用user info 获取相关信息并定向到home
+    // async function f(code:any) {
+      await loadUserInfo(code);
+    // }
+    // f(code)
+    
+    console.log("企业微信url获取的code",code)
+    redirect("/");
+  }
+  //如果是企业微信app过来的
+  const redirectUrl = encodeURIComponent(`${process.env.NEXT_PUBLIC_AUTH_WECHAT_REDIRECT_URI}/login?from=wecom`);
+  // const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_APP_ID}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_base&state=redirect#wechat_redirect`;
+  const url = `https://login.work.weixin.qq.com/wwlogin/sso/login?login_type=CorpApp&appid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_APP_ID}&agentid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_AGENT_ID}&redirect_uri=${redirectUrl}&state=redirect`;
+  // console.log(url)
+  redirect(url);
 
-  if (from) {
-    //如果是企业微信app过来的
-    if (code && state === "redirect") {
-      //如果已经有code了,则调用user info 获取相关信息并定向到home
-      const user = await loadUserInfo(code);
-      console.log("企业微信url获取的code",code)
-      router.push("/");
-    } else {
-      const redirectUrl = encodeURIComponent(`${process.env.NEXT_PUBLIC_AUTH_WECHAT_REDIRECT_URI}/login?from=wecom`);
-      const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_APP_ID}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_base&state=redirect#wechat_redirect`;
-      //const url = `https://login.work.weixin.qq.com/wwlogin/sso/login?login_type=CorpApp&appid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_APP_ID}&agentid=${process.env.NEXT_PUBLIC_AUTH_WECHAT_AGENT_ID}&redirect_uri=${redirectUrl}&state=redirect`;
-      // console.log(url)
-      router.push(url);
-    }
-  } else {
+
     let wwLogin: any;
     const wwLoginOptions: any = {
       el: "#ww_login",
@@ -47,7 +61,7 @@ export default async function login() {
         // console.log("微信code",data.code)
         const user = await loadUserInfo(data.code);
         // console.log("user",user)
-        router.push("/");
+        redirect("/");
       },
       onLoginFail(err: any) {
         console.log(err);
@@ -55,9 +69,9 @@ export default async function login() {
       },
     };
 
-    useEffect(() => {
-      wwLogin = ww.createWWLoginPanel(wwLoginOptions);
-    }, []);
-  }
+
+    wwLogin = ww.createWWLoginPanel(wwLoginOptions);
+
+  
   return <div id="ww_login" />;
 }
